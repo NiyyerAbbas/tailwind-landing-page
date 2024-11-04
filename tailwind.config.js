b3d5f5d0a42fd015f3c8ffec7bfc3928f0299f06
Niyyer Abbas <111376598+NiyyerAbbas@users.noreply.github.com>
2024-11-04 00:06:47 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'main': '#e002a2',
+      'second': '#47019d',
+      'three': '#e00256',
+      'black': '#212121',
+      'white': '#ffffff',
+      'gray': '#808080e2'
+    },
+  },
+  plugins: [],
+}
